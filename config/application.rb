25033ab4c4947a3c3ebb8a3b68307b7438a21b40
lidawei.me@gmail.com <lidawei.me@gmail.com>
2018-05-08 22:17:55 +0800

    添加用户系统样式, 添加邮箱验证, 配置邮件服务

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module Padagogy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :"zh-CN"
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.qq.com",
+      :port                 => 587,
+      :domain               => 'qq.com',
+      :user_name            => 'i@iewad.me',
+      :password             => 'wlkfrogvzrqvbehb',
+      :authentication       => "login",
+      :enable_starttls_auto => true
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
