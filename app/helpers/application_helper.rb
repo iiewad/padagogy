25368d204ff8833762e87930d3f6d9c170150bbf
lidawei.me@gmail.com <lidawei.me@gmail.com>
2018-05-10 23:04:57 +0800

    设置用户头像(gravatar), 在菜单栏和评论列表加入用户头像

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       notice:  'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
+
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.nickname, class: "gravatar")
+  end
+
 end
