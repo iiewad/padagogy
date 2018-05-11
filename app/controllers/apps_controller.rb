1f3f62a0e311bdcaa498aa791748faf68b6c1610
lidawei.me@gmail.com <lidawei.me@gmail.com>
2018-05-11 18:36:50 +0800

    App 添加评论功能

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,5 +5,10 @@ class AppsController < ApplicationController
 
   def show
     @app = App.find(params[:id])
+    if current_user
+      @new_comment = Comment.build_from(@app, current_user.id, "")
+    else
+      @new_comment = Comment.new
+    end
   end
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,2 +1,3 @@
 class App < ApplicationRecord
+  acts_as_commentable
 end
